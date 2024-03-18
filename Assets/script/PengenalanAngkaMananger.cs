83b88fda2514c377418747b91ee1ddbe6bdfda07
imeldasari25 <145621010+imeldasari25@users.noreply.github.com>
2024-03-18 11:18:30 +0700

    jaga-jaga

---
+++ Assets/script/PengenalanAngkaMananger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PengenalanAngkaMananger : MonoBehaviour
+{
+    public Transform card1;
+    public Transform card2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AssignedCard(Transform card)
+    {
+        card1 = card;
+    }
+}
