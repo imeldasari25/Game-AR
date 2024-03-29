48ae8e30acea346756b75a64c1acb6319dc656db
imeldasari25 <145621010+imeldasari25@users.noreply.github.com>
2024-03-29 10:25:47 +0700

    export angka

--- Assets/script/PengenalanAngkaMananger.cs
+++ Assets/script/PengenalanAngkaMananger.cs
@@ -6,10 +6,13 @@ public class PengenalanAngkaMananger : MonoBehaviour
 {
     public Transform card1;
     public Transform card2;
+
+    public float screenWidth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        screenWidth
     }
 
     // Update is called once per frame
