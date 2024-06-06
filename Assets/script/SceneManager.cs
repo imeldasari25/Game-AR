0f640ad3668aca99f3c637973d9bee71fc2a3610
imeldasari25 <145621010+imeldasari25@users.noreply.github.com>
2024-06-06 09:39:01 +0700

    Audio

---
+++ Assets/script/SceneManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        debug.log
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
