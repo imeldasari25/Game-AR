73249936700f38bee266463aee5340354078a083
imeldasari25 <145621010+imeldasari25@users.noreply.github.com>
2024-07-04 03:03:26 +0700

    Logo apk

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVhqVkYxdi8vLy8vQUFBQm1kRjFlMEZaNkV5amp5YVVtUjlITHNSVFB4cWRGVHhRVHhEREducDBLbXkzZlBzb1E1TlJJWmNjYXZuWUpnVU1Eb1FZZGtTYzFQQ0tyclRmcGFwWGFFbkR1Y3FJRXdKMW9YSTlpQkZwZHZhN2x2ZlNMdnpKcmRuZFNrY1hGbFZHTDg5RTRlVlJ5eVpDdkorVUVFWVl4QVJEb2RvUlJxaFZjaDFKdXRJRzQvQi9ESHExVnRRWVRlbDVkSUVuTERCRjBoazE2c3hFMTFIc1czeFRTbDJMeDJFZjRnbXUvcVMzdGtrWVp6QVZuTTdmNkZDYWZEa2ZBLytQUndzR0NocHdESThleEZ3dnVYNUJHbkI1Z2F3WjVJaFR6R3dxYklHQnYrN1NOSlQ3bkJRZ2x4VkZ4ZGNPMlc2aXdaa242MVlLcGU5cUxNNnpnQ0xiZC9Gb1RoQ0FHNzk4L3V0Q0tmeUY1N3U3dmlBQVYxb2w=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
